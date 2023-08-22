34d75b2153080aa859579446a26f50c0fe1cf0b5
bobo305 <126503694+bobo305@users.noreply.github.com>
2023-08-22 05:07:37 +0000

    all done

--- src/app.py
+++ src/app.py
@@ -38,6 +38,55 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/member' , methods=['POST'])
+def add_member():
+    age= request.json.get("age")
+    first_name= request.json.get("first_name")
+    lucky_numbers = request.json.get("lucky_numbers")
+    
+    if age is None:
+        return "missing the age", 400
+    if first_name is None:
+        return "missing the first name ", 400
+    if lucky_numbers is None:
+        return "missing the lucky_number", 400
+    
+    
+    jackson_family.add_member(
+        {    
+            "id": jackson_family._generateId(),
+            "first_name": first_name,
+            "last_name": jackson_family.last_name,
+            "age":age,
+            "lucky_number": lucky_numbers
+        }
+    )
+    
+    return "member has been added" ,200
+
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_family_member(id):
+    jackson_family.delete_member(id)
+    response={"family member has been deleted ": True}
+    return jsonify(response), 200
+   
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_individual_family_member(id):
+    member = jackson_family.get_member(id)
+    response= member
+    return jsonify(response), 200
+
+
+
+# @app.route('/member/<int:id>', methods=['GET'])
+# def get_individual_family_member(id):
+#     jackson_family.get_member(id)
+#     response_body =add_member
+
+#     return jsonify(response_body), 200
+    
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
